70b49b054d9318d85c691c7ebe47557c33edacb6
George <gio@gmail.com>
2022-06-05 00:20:51 +0400

    feat(auth): add redirection to auth page when user enters wrong route by hand

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
 import Login from "./features/auth/Login";
 import SignupClient from "./features/auth/SignupClient";
 import SignupBarber from "./features/auth/SignupBarber";
-
+import { AuthorizedRoutes, UnauthorizedRoutes } from './features/auth/Guards';
 import { useState } from "react";
 import {
   BrowserRouter,
   Routes,
   Route,
+  Navigate
 } from "react-router-dom";
 import './App.css';
+import Dashboard from "./features/Dashboard";
 
 function App() {
   const [activeUser, setActiveUser] = useState(null);
 
   const addActiveUser = (user) => {
-    console.log(user);
     setActiveUser(user);
   }
-
-
 
   return (
     <BrowserRouter>
         <div className="containerCustom">
           <Routes>
-              <Route path="auth/login" element={<Login  addActiveUser={addActiveUser}/>} />
-              <Route path="auth/signup" element={<SignupClient addActiveUser={addActiveUser}/>} />
-              <Route path="auth/signup-barber" element={<SignupBarber />} />
+              <Route path='*' element={<Navigate to="/auth/login"/>} />
+              <Route path="dashboard" element={
+                <AuthorizedRoutes user={activeUser}>
+                  <Dashboard/>
+                </AuthorizedRoutes>
+              }></Route>
+              <Route path="auth/login" element={
+                  <UnauthorizedRoutes user={activeUser}>
+                    <Login  addActiveUser={addActiveUser}/>
+                  </UnauthorizedRoutes> }
+               />
+              <Route path="auth/signup" element={
+                <UnauthorizedRoutes user={activeUser}>
+                  <SignupClient addActiveUser={addActiveUser}/>
+                </UnauthorizedRoutes>
+              } />
+              <Route path="auth/signup-barber" element={
+                <UnauthorizedRoutes user={activeUser}>
+                  <SignupBarber />
+                </UnauthorizedRoutes>
+              } />
           </Routes>
         </div>
     </BrowserRouter>
